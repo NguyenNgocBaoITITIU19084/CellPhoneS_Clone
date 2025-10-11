6cb68f6bdd47356a766e50f73451018432a20b79
Bao Nguyen <ititiu19084@student.hcmiu.edu.vn>
2025-10-11 15:01:22 +0700

    completed-banner-ui

--- vite-cellphoneS-web-base-project-master/src/components/Banner/Banner.jsx
+++ vite-cellphoneS-web-base-project-master/src/components/Banner/Banner.jsx
@@ -1,5 +1,27 @@
+import styled from "@emotion/styled"
+import Box from "@mui/material/Box"
+
+const ContainerBox = styled(Box)({
+  maxWidth: '1200px',
+  margin: 'auto',
+  maxHeight: '75px',
+  marginTop: '20px',
+  width: '1200px', 
+  height: '75px'
+})
+
 export default function Banner() {
   return (
-    <div>Banner</div>
+    <ContainerBox>
+      <Box
+        sx={{
+          borderRadius: '5px'
+        }}
+        component='img' 
+        src="https://cdn2.cellphones.com.vn/insecure/rs:fill:1200:75/q:90/plain/https://dashboard.cellphones.com.vn/storage/special-b2s-dday3-d.gif" 
+        alt="Image banner of Homepage"
+      >
+      </Box>
+    </ContainerBox>
   )
 }
--- vite-cellphoneS-web-base-project-master/src/pages/HomePage/index.jsx
+++ vite-cellphoneS-web-base-project-master/src/pages/HomePage/index.jsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box"
 import AppBar from "~/components/AppBoard"
+import Banner from "~/components/Banner/Banner"
 import Hero from "~/components/Hero/Hero"
 
 function HomePage() {
@@ -7,6 +8,7 @@ function HomePage() {
     <Box sx={{ backgroundColor: 'white', height: '100vh', width: '100%' }}>
         <AppBar />
         <Hero />
+        <Banner/>
     </Box>
   )
 }
